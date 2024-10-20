53a5b9387835f4c56e86fb4ffac65422ba9ae5fe
Alexander Lytovchenko <flopsi69@gmail.com>
2024-10-20 15:57:52 +0700

    remove ExitIntent

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,88 +1,88 @@
 <script lang="ts" setup>
 const { isOpen, modalData, closeModal } = useModal();
-const { isOpen: isExitIntentOpen, closeModal: closeExitIntent } = useExitIntent();
+// const { isOpen: isExitIntentOpen, closeModal: closeExitIntent } = useExitIntent();
 
-let lastScrollY = 0;
-let actionCounter = 0;
-let actionInterval: any;
-let scrollTimeout: any;
+// let lastScrollY = 0;
+// let actionCounter = 0;
+// let actionInterval: any;
+// let scrollTimeout: any;
 
-function closeModalHandler(isCheckExitIntent = false) {
-  closeModal();
+// function closeModalHandler(isCheckExitIntent = false) {
+//   closeModal();
 
-  if (isCheckExitIntent) {
-    showExitIntent();
-  }
-}
+//   if (isCheckExitIntent) {
+//     showExitIntent();
+//   }
+// }
 
-function showExitIntent() {
-  if (sessionStorage.getItem('exitIntentShown') || isExitIntentOpen.value || isOpen.value) return;
+// function showExitIntent() {
+//   if (sessionStorage.getItem('exitIntentShown') || isExitIntentOpen.value || isOpen.value) return;
 
-  clearInterval(actionInterval);
-  sessionStorage.setItem('exitIntentShown', 'true');
-  isExitIntentOpen.value = true;
-}
+//   clearInterval(actionInterval);
+//   sessionStorage.setItem('exitIntentShown', 'true');
+//   isExitIntentOpen.value = true;
+// }
 
 // Desktop: Detect mouse leaving the window
-function handleMouseLeave(event: MouseEvent) {
-  if (event.clientY <= 0 && !sessionStorage.getItem('exitIntentShown') && window.innerWidth > 768) {
-    showExitIntent();
-  }
-}
+// function handleMouseLeave(event: MouseEvent) {
+//   if (event.clientY <= 0 && !sessionStorage.getItem('exitIntentShown') && window.innerWidth > 768) {
+//     showExitIntent();
+//   }
+// }
 
 // Mobile: Detect fast scroll up
-function handleScroll() {
-  actionCounter = 0;
+// function handleScroll() {
+//   actionCounter = 0;
 
-  if (window.innerWidth > 768) return;
+//   if (window.innerWidth > 768) return;
 
-  const currentScrollY = window.scrollY;
-  const scrollSpeed = currentScrollY - lastScrollY;
+//   const currentScrollY = window.scrollY;
+//   const scrollSpeed = currentScrollY - lastScrollY;
 
-  if (Math.abs(scrollSpeed) > 150 && !sessionStorage.getItem('exitIntentShown')) {
-    showExitIntent();
-  }
+//   if (Math.abs(scrollSpeed) > 150 && !sessionStorage.getItem('exitIntentShown')) {
+//     showExitIntent();
+//   }
 
-  lastScrollY = currentScrollY;
+//   lastScrollY = currentScrollY;
 
-  if (scrollTimeout) clearTimeout(scrollTimeout);
+//   if (scrollTimeout) clearTimeout(scrollTimeout);
 
-  scrollTimeout = setTimeout(() => {
-    lastScrollY = window.scrollY;
-  }, 100);
-};
+//   scrollTimeout = setTimeout(() => {
+//     lastScrollY = window.scrollY;
+//   }, 100);
+// };
 
 // Reset action counter on user interaction
-function resetActionCounter() {
-  actionCounter = 0;
-}
+// function resetActionCounter() {
+//   actionCounter = 0;
+// }
 
-onMounted(() => {
-  if (sessionStorage.getItem('exitIntentShown')) return;
+// onMounted(() => {
+//   if (sessionStorage.getItem('exitIntentShown')) return;
 
-  actionInterval = setInterval(() => {
-    actionCounter += 1;
+//   actionInterval = setInterval(() => {
+//     actionCounter += 1;
 
-    if (actionCounter > 30) {
-      showExitIntent();
-      clearInterval(actionInterval);
-    }
-  }, 1000);
+//     if (actionCounter > 30) {
+//       showExitIntent();
+//       clearInterval(actionInterval);
+//     }
+//   }, 1000);
 
 
-  setTimeout(() => {
-    lastScrollY = window.scrollY;
-    document.addEventListener('mouseout', handleMouseLeave);
-    window.addEventListener('scroll', handleScroll);
-    document.addEventListener('click', resetActionCounter);
-  }, 3000);
-});
+//   setTimeout(() => {
+//     lastScrollY = window.scrollY;
+//     document.addEventListener('mouseout', handleMouseLeave);
+//     window.addEventListener('scroll', handleScroll);
+//     document.addEventListener('click', resetActionCounter);
+//   }, 3000);
+// });
 
-onUnmounted(() => {
-  document.removeEventListener('mouseout', handleMouseLeave);
-  window.removeEventListener('scroll', handleScroll);
-  document.removeEventListener('click', resetActionCounter);
-});
+// onUnmounted(() => {
+//   document.removeEventListener('mouseout', handleMouseLeave);
+//   window.removeEventListener('scroll', handleScroll);
+//   document.removeEventListener('click', resetActionCounter);
+// });
 </script>
 
 <template>
@@ -96,17 +96,17 @@ onUnmounted(() => {
         <transition name="overlay-fade">
           <BaseModal
             v-bind="modalData"
-            @close-modal="closeModalHandler"
+            @close-modal="closeModal"
             v-if="isOpen"
           />
         </transition>
 
-        <transition name="overlay-fade">
+        <!-- <transition name="overlay-fade">
           <BaseExitIntent
             v-if="isExitIntentOpen"
             @close-modal="closeExitIntent"
           />
-        </transition>
+        </transition> -->
       </Teleport>
     </ClientOnly>
   </div>
