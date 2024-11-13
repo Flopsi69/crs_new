e49b331fc89c77b6030c30b614ad3f18ec27bdae
Svetlana Khvastova <svetakhvastova@gmail.com>
2024-11-13 10:18:02 +0200

    test

---
+++ pages/testCase.vue
@@ -0,0 +1,292 @@
+<script lang="ts" setup></script>
+
+<template>
+    <div>
+        <tCase1 bgClass="bg_light_violet" borderClass="border_violet" alertClass="alert_big" iconClass="yellow_icon">
+            <div>
+                <svg class="alert_icon" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40"
+                    fill="none">
+                    <g clip-path="url(#clip0_283_1294)">
+                        <path
+                            d="M20 0C8.97167 0 0 8.97167 0 20C0 31.0283 8.97167 40 20 40C31.0283 40 40 31.0283 40 20C40 8.97167 31.0283 0 20 0ZM20 31.6667C19.08 31.6667 18.3333 30.92 18.3333 30C18.3333 29.08 19.08 28.3333 20 28.3333C20.92 28.3333 21.6667 29.08 21.6667 30C21.6667 30.92 20.92 31.6667 20 31.6667ZM21.6667 23.3333C21.6667 24.2533 20.92 25 20 25C19.08 25 18.3333 24.2533 18.3333 23.3333V10C18.3333 9.08 19.08 8.33333 20 8.33333C20.92 8.33333 21.6667 9.08 21.6667 10V23.3333Z"
+                            fill="#E3AA10" />
+                    </g>
+                    <defs>
+                        <clipPath id="clip0_283_1294">
+                            <rect width="40" height="40" fill="white" />
+                        </clipPath>
+                    </defs>
+                </svg>
+            </div>
+            <div>
+                <h3 class="alert_title">For the average store?</h3>
+                <p class="alert_description">52 out of 100 shoppers will browse, fill their cart, and act like they’re
+                    about to checkout, only to turn around at the last minute and waltz out of your virtual store –
+                    never to return again. But imagine the revenue increase you could generate if you were to keep even
+                    a few of those customers from leaving…</p>
+            </div>
+        </tCase1>
+
+        <tCase1 bgClass="bg_dark_violet" borderClass="border_violet" alertClass="alert_big" iconClass="yellow_icon">
+            <div>
+                <svg class="alert_icon" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40"
+                    fill="none">
+                    <g clip-path="url(#clip0_283_1294)">
+                        <path
+                            d="M20 0C8.97167 0 0 8.97167 0 20C0 31.0283 8.97167 40 20 40C31.0283 40 40 31.0283 40 20C40 8.97167 31.0283 0 20 0ZM20 31.6667C19.08 31.6667 18.3333 30.92 18.3333 30C18.3333 29.08 19.08 28.3333 20 28.3333C20.92 28.3333 21.6667 29.08 21.6667 30C21.6667 30.92 20.92 31.6667 20 31.6667ZM21.6667 23.3333C21.6667 24.2533 20.92 25 20 25C19.08 25 18.3333 24.2533 18.3333 23.3333V10C18.3333 9.08 19.08 8.33333 20 8.33333C20.92 8.33333 21.6667 9.08 21.6667 10V23.3333Z"
+                            fill="#E3AA10" />
+                    </g>
+                    <defs>
+                        <clipPath id="clip0_283_1294">
+                            <rect width="40" height="40" fill="white" />
+                        </clipPath>
+                    </defs>
+                </svg>
+            </div>
+            <div>
+                <h3 class="alert_title">For the average store?</h3>
+                <p class="alert_description">52 out of 100 shoppers will browse, fill their cart, and act like they’re
+                    about to checkout, only to turn around at the last minute and waltz out of your virtual store –
+                    never to return again. But imagine the revenue increase you could generate if you were to keep even
+                    a few of those customers from leaving…</p>
+            </div>
+        </tCase1>
+
+        <tCase1 bgClass="bg_white" borderClass="border_violet" alertClass="alert_small" iconClass="violet_icon">
+            <div>
+                <svg class="alert_icon" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40"
+                    fill="none">
+                    <g clip-path="url(#clip0_283_1294)">
+                        <path
+                            d="M20 0C8.97167 0 0 8.97167 0 20C0 31.0283 8.97167 40 20 40C31.0283 40 40 31.0283 40 20C40 8.97167 31.0283 0 20 0ZM20 31.6667C19.08 31.6667 18.3333 30.92 18.3333 30C18.3333 29.08 19.08 28.3333 20 28.3333C20.92 28.3333 21.6667 29.08 21.6667 30C21.6667 30.92 20.92 31.6667 20 31.6667ZM21.6667 23.3333C21.6667 24.2533 20.92 25 20 25C19.08 25 18.3333 24.2533 18.3333 23.3333V10C18.3333 9.08 19.08 8.33333 20 8.33333C20.92 8.33333 21.6667 9.08 21.6667 10V23.3333Z"
+                            fill="#E3AA10" />
+                    </g>
+                    <defs>
+                        <clipPath id="clip0_283_1294">
+                            <rect width="40" height="40" fill="white" />
+                        </clipPath>
+                    </defs>
+                </svg>
+            </div>
+            <div>
+                <p class="alert_description">Friction — layout issues like that are bound to cause people to churn out
+                    of frustration – so we deducted our first point.</p>
+            </div>
+        </tCase1>
+
+        <tCase1 bgClass="bg_dark_violet" borderClass="border_violet" alertClass="alert_small" iconClass="yellow_icon">
+            <div>
+                <svg class="alert_icon" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40"
+                    fill="none">
+                    <g clip-path="url(#clip0_283_1294)">
+                        <path
+                            d="M20 0C8.97167 0 0 8.97167 0 20C0 31.0283 8.97167 40 20 40C31.0283 40 40 31.0283 40 20C40 8.97167 31.0283 0 20 0ZM20 31.6667C19.08 31.6667 18.3333 30.92 18.3333 30C18.3333 29.08 19.08 28.3333 20 28.3333C20.92 28.3333 21.6667 29.08 21.6667 30C21.6667 30.92 20.92 31.6667 20 31.6667ZM21.6667 23.3333C21.6667 24.2533 20.92 25 20 25C19.08 25 18.3333 24.2533 18.3333 23.3333V10C18.3333 9.08 19.08 8.33333 20 8.33333C20.92 8.33333 21.6667 9.08 21.6667 10V23.3333Z"
+                            fill="#E3AA10" />
+                    </g>
+                    <defs>
+                        <clipPath id="clip0_283_1294">
+                            <rect width="40" height="40" fill="white" />
+                        </clipPath>
+                    </defs>
+                </svg>
+            </div>
+            <div>
+                <p class="alert_description">Friction — layout issues like that are bound to cause people to churn out
+                    of frustration – so we deducted our first point.</p>
+            </div>
+        </tCase1>
+
+        <tCase1 bgClass="bg_light_violet" borderClass="border_violet" alertClass="quote_big" iconClass="yellow_icon">
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none">
+                    <path
+                        d="M5.93928 31.3762C4.01312 29.1808 2.85742 26.7859 2.85742 22.7943C2.85742 15.809 7.67283 9.62199 14.4144 6.42871L16.148 9.02325C9.79161 12.6157 8.4433 17.206 8.05806 20.1997C9.02115 19.601 10.3695 19.4014 11.7178 19.601C15.1849 20.0001 17.8815 22.7943 17.8815 26.5863C17.8815 28.3825 17.111 30.1787 15.9553 31.5758C14.607 32.9728 13.0661 33.5716 11.1399 33.5716C9.02115 33.5716 7.09498 32.5737 5.93928 31.3762ZM25.2009 31.3762C23.2748 29.1808 22.1191 26.7859 22.1191 22.7943C22.1191 15.809 26.9345 9.62199 33.676 6.42871L35.4096 9.02325C29.0532 12.6157 27.7049 17.206 27.3197 20.1997C28.2828 19.601 29.6311 19.4014 30.9794 19.601C34.4465 20.0001 37.1431 22.7943 37.1431 26.5863C37.1431 28.3825 36.3727 30.1787 35.217 31.5758C34.0613 32.9728 32.3277 33.5716 30.4016 33.5716C28.2828 33.5716 26.3566 32.5737 25.2009 31.3762Z"
+                        fill="#E3AA10" />
+                </svg>
+            </div>
+            <div>
+                <p class="quote_description">As expected, a significant number of users abandoned their Gaiam cart
+                    during the final stages of the checkout process. But why was this happening? A deep dive into the
+                    user journey revealed several potential causes:</p>
+            </div>
+        </tCase1>
+
+        <tCase1 bgClass="bg_dark_violet" borderClass="border_violet" alertClass="alert_small" iconClass="yellow_icon">
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none">
+                    <path
+                        d="M5.93928 31.3762C4.01312 29.1808 2.85742 26.7859 2.85742 22.7943C2.85742 15.809 7.67283 9.62199 14.4144 6.42871L16.148 9.02325C9.79161 12.6157 8.4433 17.206 8.05806 20.1997C9.02115 19.601 10.3695 19.4014 11.7178 19.601C15.1849 20.0001 17.8815 22.7943 17.8815 26.5863C17.8815 28.3825 17.111 30.1787 15.9553 31.5758C14.607 32.9728 13.0661 33.5716 11.1399 33.5716C9.02115 33.5716 7.09498 32.5737 5.93928 31.3762ZM25.2009 31.3762C23.2748 29.1808 22.1191 26.7859 22.1191 22.7943C22.1191 15.809 26.9345 9.62199 33.676 6.42871L35.4096 9.02325C29.0532 12.6157 27.7049 17.206 27.3197 20.1997C28.2828 19.601 29.6311 19.4014 30.9794 19.601C34.4465 20.0001 37.1431 22.7943 37.1431 26.5863C37.1431 28.3825 36.3727 30.1787 35.217 31.5758C34.0613 32.9728 32.3277 33.5716 30.4016 33.5716C28.2828 33.5716 26.3566 32.5737 25.2009 31.3762Z"
+                        fill="#E3AA10" />
+                </svg>
+            </div>
+            <div>
+                <p class="quote_description">As expected, a significant number of users abandoned their Gaiam cart
+                    during the final stages of the checkout process. But why was this happening? A deep dive into the
+                    user journey revealed several potential causes:</p>
+            </div>
+        </tCase1>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.border_violet {
+    border: 2px solid #ede8f6;
+}
+
+.alert_big {
+    gap: 16px;
+
+    .alert_icon {
+        width: 40px;
+        height: 40px;
+    }
+}
+
+.alert_small {
+    gap: 12px;
+
+    .alert_icon {
+        width: 28px;
+        height: 28px;
+    }
+}
+
+.yellow_icon {
+    path {
+        fill: $yellow;
+    }
+}
+
+.violet_icon {
+    path {
+        fill: $purple;
+    }
+}
+
+.alert_title {
+    font-size: 24px;
+    line-height: 130%;
+    margin: 0 0 12px;
+}
+
+.alert_description {
+    font-size: 18px;
+    line-height: 160%;
+}
+
+.quote_big {
+    gap: 16px;
+}
+
+.quote_description {
+    font-size: 18px;
+    line-height: 160%;
+    font-weight: 700;
+}
+
+.bg_dark_violet {
+    background: $violet;
+
+    .box_description {
+        color: #fff;
+    }
+
+    .box_item {
+        background: $bg--purple-light;
+    }
+
+    .box_percent {
+        color: $violet;
+    }
+
+    .box_label {
+        color: $font-primary;
+    }
+
+    .alert_title {
+        color: #fff;
+    }
+
+    .alert_description {
+        color: #fff;
+    }
+
+    .quote_description {
+        color: #fff;
+    }
+
+    &.list_wrapper .list.ordered_list .list_item {
+        color: #fff;
+    }
+
+    &.list_wrapper .unordered_list .list_item {
+        color: $yellow;
+    }
+
+    &.list_wrapper .list_item * {
+        color: #fff;
+    }
+}
+
+.bg_light_violet {
+    background: $bg--purple-light;
+
+    .box_description {
+        color: $violet;
+    }
+
+    .box_item {
+        background: $purple;
+    }
+
+    .box_percent {
+        color: #fff;
+    }
+
+    .box_label {
+        color: #fff;
+    }
+
+    .alert_title {
+        color: $font-primary;
+    }
+
+    .alert_description {
+        color: $font-secondary;
+    }
+
+    .quote_description {
+        color: $font-primary;
+    }
+}
+
+.bg_white {
+    background: #fff;
+
+    .alert_description {
+        color: $font-secondary;
+    }
+}
+
+@media (max-width: $sm) {
+    .alert_big {
+        gap: 0;
+        flex-direction: row-reverse;
+
+        .alert_icon {
+            width: 24px;
+            height: 24px;
+        }
+    }
+
+    .quote_big {
+        gap: 0;
+        flex-direction: row-reverse;
+
+        svg {
+            width: 28px;
+            height: 28px;
+        }
+    }
+}
+</style>
