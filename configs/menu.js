acb8b99d8b3e72adf44660858aaf527682814390
Alexander Lytovchenko <flopsi69@gmail.com>
2025-01-08 20:30:13 +0700

    add nav link control

--- configs/menu.js
+++ configs/menu.js
@@ -1,23 +1,28 @@
 export default [
   {
     title: 'Ecommerce CRO Agency',
-    url: 'https://conversionrate.store/ecommerce-cro-agency/'
+    url: 'https://conversionrate.store/ecommerce-cro-agency/',
+    isActive: false
   },
   {
     title: 'CRO Case Studies',
-    url: '/case-studies/'
+    url: 'https://conversionrate.store/cro-case-studies/',
+    isActive: true
   },
   {
     title: 'Performance-based CRO',
-    url: 'https://conversionrate.store/performance-based-cro/'
+    url: 'https://conversionrate.store/performance-based-cro/',
+    isActive: false
   },
   {
     title: 'CRO retainer',
-    url: 'https://conversionrate.store/cro-retainer/'
+    url: 'https://conversionrate.store/cro-retainer/',
+    isActive: false
   },
   {
     title: 'CRO audit',
-    url: 'https://conversionrate.store/cro-audit/'
+    url: 'https://conversionrate.store/cro-audit/',
+    isActive: false
   },
   // {
   //   title: 'Careers',
@@ -25,6 +30,7 @@ export default [
   // },
   {
     title: 'Contact us',
-    url: 'https://conversionrate.store/contact-us/'
+    url: 'https://conversionrate.store/contact-us/',
+    isActive: true
   }
 ]
