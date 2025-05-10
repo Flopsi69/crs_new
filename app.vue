143bd0e669e73fa12cef74c84d2df85b96096996
Alexander Lytovchenko <flopsi69@gmail.com>
2025-05-10 15:30:33 +0700

    add locale class to body

--- app.vue
+++ app.vue
@@ -1,9 +1,13 @@
 <script lang="ts" setup>
 const i18nHead = useLocaleHead()
+const { locale } = useI18n()
 
 useHead(() => ({
   htmlAttrs: {
     lang: i18nHead.value.htmlAttrs.lang
+  },
+  bodyAttrs: {
+    class: 'lang-' + locale.value
   },
   link: [...(i18nHead.value.link || [])],
   meta: [...(i18nHead.value.meta || [])]
